22a2aec60ffd977f4ba19b27925578cb1b4419d2
lilei0331 <lilei0331@thundersoft.com>
2015-04-25 11:02:16 +0800

    init c socket client and server function:send socket message and recevie socket message

---
+++ client.c
@@ -0,0 +1,101 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  void sendJson(int sd);
+  void getOneApp(int sd);
+  printf("test clent~~ \n");
+  struct sockaddr_in pin;
+  struct hostent *nlp_host;
+  int sd; 
+  char host_name[256];
+  int port;
+  char newLine[1] = "\n";
+  char msgState[] = "100";
+  strcat(msgState,newLine);    //添加换行符
+  char packageNmae[] = "com.android.browser";
+  strcat(packageNmae,newLine); //添加换行符  
+  char className[] ="com.android.browser.BrowserActivity"; 
+
+  //getOneApp(msgState,className); //手机wifi ip 192.168.31.81
+  //初始化主机名和端口。主机名可以是IP，也可以是可被解析的名称localhost
+  //strcpy(host_name,"localhost");
+  strcpy(host_name,"192.168.31.81");
+  port=4700;
+ 
+  //解析域名，如果是IP则不用解析，如果出错，显示错误信息
+  while((nlp_host=gethostbyname(host_name))==0){
+    printf("Resolve Error!\n");
+  }
+  printf("1111 connecting:%s port:%d\n",host_name,port);
+  //设置pin变量，包括协议、地址、端口等，此段可直接复制到自己的程序中
+  bzero(&pin,sizeof(pin));
+  pin.sin_family=AF_INET;                 //AF_INET表示使用IPv4
+  pin.sin_addr.s_addr=htonl(INADDR_ANY);  
+  pin.sin_addr.s_addr=((struct in_addr *)(nlp_host->h_addr))->s_addr;
+  pin.sin_port=htons(port);
+ 
+  //建立socket
+  sd=socket(AF_INET,SOCK_STREAM,0);
+ 
+  //建立连接
+  if(connect(sd,(struct sockaddr*)&pin,sizeof(pin))==-1){
+    printf("Connect Error!\n");
+  } 
+/*  send(sd,msgState,sizeof(msgState),0);       //发送configState
+  send(sd,className,sizeof(className),0);     //发送类信息 */
+  //getOneApp(sd);
+  sendJson(sd); 
+  printf("2222 end\n");
+}
+void sendJson(int sd){
+  char msgState[] = "106";//111 start app
+  char local[] = "zh_CN";
+  char packageName[] = "com.android.settings";
+  char className[] = "com.android.settings.Settings"; 
+  char test[] ="{\":\"}:::{}}}{{{,,,,::""";
+  char jsonStr1[] ="{\"messageType\":104}"; //104 get all
+  char jsonStr2[]= "{\"messageType\":130,\"timeInMillis\":1420078210000}";
+  char jsonStr[] ="{\"messageType\":111,\"packageName\":\"com.android.contacts\",\"activityName\":\"com.android.contacts.activities.PeopleActivity\"}";
+  char jsonStr4[] = "{\"messageType\":129,\"language\":\"zh\",\"area\":\"CN\"}";
+
+  send(sd,jsonStr,sizeof(jsonStr),0);       //发送configState
+  printf("sendJson() json size:%ld json str:%s\n",sizeof(jsonStr),jsonStr);
+
+}
+void getOneApp(int sd){
+  char newLine[1] = "\n";
+  char msgState[] = "111";//111 start app
+  char local[] = "zh_CN";
+  char time[] = "1286676610000"; // 2010-10-10 10:10:10
+  char packageName[] = "com.android.settings";
+  char className[] = "com.android.settings.Settings"; 
+  int lenP = sizeof(packageName);
+  int lenC = sizeof(className);
+  char arrLenP[3];  //若是两位数 必须是3，否则android端无法接收数据
+  char arrLenC[3];
+  sprintf(arrLenP,"%d",lenP);   //int 转换为String
+  sprintf(arrLenC,"%d",lenC);
+
+  strcat(msgState,newLine);  
+  strcat(arrLenP,newLine);
+  strcat(arrLenC,newLine);
+  strcat(packageName,newLine);
+
+  send(sd,msgState,sizeof(msgState),0);       //发送configState
+//  send(sd,time,sizeof(time),0);      //设置时间
+//  send(sd,local,sizeof(local),0);  //设置语言
+  send(sd,arrLenP,sizeof(arrLenP),0);           //发送包长度
+  send(sd,arrLenC,sizeof(arrLenC),0);           //发送包长度
+  send(sd,packageName,sizeof(packageName),0);     //发送类信息
+  send(sd,className,sizeof(className),0);     //发送类信息  */ 
+  printf("getOneApp() state size:%ld arrLenP:%s arrLenC:%s \n",sizeof(msgState),arrLenP,arrLenC);
+}
---
+++ server.c
@@ -0,0 +1,108 @@
+ 
+//gcc -o hello hello.c 
+
+#include <netinet/in.h>    // for sockaddr_in
+#include <sys/types.h>    // for socket
+#include <sys/socket.h>    // for socket
+#include <stdio.h>        // for printf
+#include <stdlib.h>        // for exit
+#include <string.h>        // for bzero
+/*
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+*/
+#define HELLO_WORLD_SERVER_PORT    8001
+#define LENGTH_OF_LISTEN_QUEUE 20
+#define BUFFER_SIZE 30000
+#define BUFFER_LIMIT 1448 
+#define FILE_NAME_MAX_SIZE 512
+ 
+int main(int argc, char **argv)
+{
+    //设置一个socket地址结构server_addr,代表服务器internet地址, 端口
+    struct sockaddr_in server_addr;
+    bzero(&server_addr,sizeof(server_addr)); 
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    server_addr.sin_port = htons(HELLO_WORLD_SERVER_PORT);
+ 
+    printf("main~~~~! \n");
+    //创建用于internet的流协议(TCP)socket,用server_socket代表服务器socket
+    int server_socket = socket(PF_INET,SOCK_STREAM,0);
+    if( server_socket < 0)
+    {
+        printf("Create Socket Failed! \n");
+        exit(1);
+    }
+{
+   int opt =1;
+   setsockopt(server_socket,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+}
+     
+    //把socket和socket地址结构联系起来
+    if( bind(server_socket,(struct sockaddr*)&server_addr,sizeof(server_addr)))
+    {
+        printf("Server Bind Port : %d Failed! \n", HELLO_WORLD_SERVER_PORT);
+        exit(1);
+    }
+ 
+    //server_socket用于监听
+    if ( listen(server_socket, LENGTH_OF_LISTEN_QUEUE) )
+    {
+        printf("Server Listen Failed!\n");
+        exit(1);
+    }
+    while (1) //服务器端要一直运行
+    {
+        //定义客户端的socket地址结构client_addr
+        struct sockaddr_in client_addr;
+        socklen_t length = sizeof(client_addr);
+        printf("wait for connecting...\n");
+        int new_server_socket = accept(server_socket,(struct sockaddr*)&client_addr,&length);
+        if ( new_server_socket < 0)
+        {
+            printf("Server Accept Failed!\n");
+            break;
+        }
+        short read = 1;
+        char buffer[BUFFER_SIZE];
+        bzero(buffer, BUFFER_SIZE);
+        //add for by lilei begin
+	while(read){
+	   printf(">>>> recv() 111 \n");
+           length = recv(new_server_socket,buffer,BUFFER_SIZE,0);
+           if(length < BUFFER_LIMIT)
+             read = 0;
+           printf(">>>> recv() length:%d,read:%d \n",length,read);
+           if (length < 0)
+           {
+             printf("Server Recieve Data Failed!\n");
+             break;
+           }else{
+             printf(">>>>received data is : %d\n ", length );
+             int i = 0; 
+             for(i; i < length; i++)
+             printf(">>>data is %d\n", buffer[i]);
+           }
+           printf(">>>> recv()222 \n");   
+
+        }
+        //add by lilei end
+        /*  if (length < 0)
+        {
+            printf("Server Recieve Data Failed!\n");
+            break;
+        }else{
+
+           printf(">>>>received data is : %d\n ", length );
+           int i = 0; 
+           for(i; i < length; i++)
+               printf(">>>data is %d\n", buffer[i]);
+        }   */
+        close(new_server_socket);
+    }
+    close(server_socket);
+    return 0;
+}
